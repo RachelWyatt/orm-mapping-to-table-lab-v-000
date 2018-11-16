b515e4fa0f6af2305338218c073c6d296d81011b
Rachel Wyatt <rw126@evansville.edu>
2018-11-16 15:41:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,12 @@
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
   
   def student(id=nil, name, grade)
-end
+    @id = some_id
+    @name = name
+    @grade = grade
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
